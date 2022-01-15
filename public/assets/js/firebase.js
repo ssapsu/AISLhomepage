d127564b643526beddbae11bf47264733a6f04a7
Hyeonsu Oh <hans324oh@gmail.com>
2022-01-15 19:10:38 +0900

    gitignore 재작성

--- public/assets/js/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDfQofvlUKDalB3J0V4A2aLKQ2zH-JMl8o",
-    authDomain: "aislhomepage.firebaseapp.com",
-    storageBucket: "gs://aislhomepage.appspot.com/",
-    projectId: "aislhomepage"
-}
-
-
--- public/assets/js/mailConfig.js
+++
@@ -1,3 +0,0 @@
-const serviceID = "service_dmfgktn";
-const userID = "user_3VOUO3tldG7hLXHhh2Za7";
-const templateID = "template_vltnb7u";
